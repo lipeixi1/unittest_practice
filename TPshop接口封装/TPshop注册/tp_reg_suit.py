ff375bb40a406bc6ffeccf2753ccbec4dc7b7cbf
Tin <2423836338@qq.com>
2025-05-05 16:00:48 +0800

    5.5 second commit

--- TPshop接口封装/TPshop注册/tp_reg_suit.py
+++ TPshop接口封装/TPshop注册/tp_reg_suit.py
@@ -8,6 +8,7 @@ from tp_reg_test import Test_tpshop_register
 from htmltestreport import HTMLTestReport
 
 if __name__ == '__main__':
+    # 实例化suit对象，添加测试方法，指定报告生成文件，然后run
     suit = unittest.TestSuite()
     suit.addTest(unittest.makeSuite(Test_tpshop_register))
     runner = HTMLTestReport('./tpshop注册接口测试.html')
